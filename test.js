487531ee7c5912c1b2fc74ceb5179296138363b2
camilavasquez <cvasquezdr7@gmail.com>
2022-01-13 13:15:26 -0500

    Failing 2 tests.

---
+++ test.js
@@ -0,0 +1,3 @@
+Test.assertSimilar(bucketize("she sells sea shells by the sea", 2), [])
+Test.assertSimilar(bucketize("ab bc cd", 1), [])
+Test.assertSimilar(bucketize("the mouse jumped over the cheese", 7));
